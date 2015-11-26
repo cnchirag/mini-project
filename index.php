01c878f5e448a7111bd574e95c878784824af87d
CHIRAG NASWA <cnchirag@users.noreply.github.com>
2015-11-26 21:49:00 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>babyname.geu.ac.in</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="nivo-slider.css" type="text/css" media="screen" />
+</head>
+<body>
+<div id="bg">
+<div id="logo"><a href="#">CHIRAG NASWA</a>
+    <h2><a href="#">Student | Information Technology | GEU | Dehradun </a></h2></div>
+<div id="main">
+<!-- header begins -->
+<div id="header"> 
+  
+  <div id="slider-wrapper">        
+                       <div id="slider" class="nivoSlider">
+                  <img src="images/header.jpg" alt="" title="" />
+                  <img src="images/header2.jpg" alt="" title="" />
+                  <img src="images/header3.jpg" alt="" title="" />
+                  <img src="images/header4.jpg" alt="" title="" />                  
+                </div>        
+                    </div>
+                
+<script type="text/javascript" src="lib/jquery-1.4.3.min.js"></script>
+    <script type="text/javascript" src="lib/jquery.nivo.slider.pack.js"></script>
+    <script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+  
+</div>
+        <div id="buttons">
+    <ul>
+      <li class="first"><a href="#"  title="">Home</a></li>
+      <li><a href="byyear.php" title="">Name by year</a></li>
+      <li><a href="byname.php" title="">Popularity</a></li>
+      <li><a href="about.html" title="">About</a></li>
+      <li><a href="contact.html" title="">Contact</a></li>
+    </ul></div>
+<!-- header ends -->
+<!-- content begins -->
+<div id="content_bg">
+    <div id="column_box">
+      <div id="bg_left"></div>
+      <div>
+        <h1>Introduction to Miniproject</h1>
+              
+      </div>
+      
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+      
+                          
+
+</div>
+<!-- content ends -->
+<!-- footer begins -->
+<div id="footer">
+  <p>Copyright  2015. Designed by <a href="#" title="Free Web Templates">Chirag Naswa</a>
+    <p><a href="#">Mini Project</a> | <a href="#">5 Sem</a> | <a href="#" title=""><abbr title="eXtensible HyperText Markup Language">2015-2016</abbr></a>  <a href="http://jigsaw.w3.org/css-validator/check/referer" title="This page validates as CSS"><abbr title="Cascading Style Sheets"></abbr></a></p></div>
+<!-- footer ends -->
+</div>
+</div>
+</body>
+</html>
